2c85defa7b595b20c21a7286a68c7e664247ab8a
DiegoFleitas <diego.fleitas68@gmail.com>
2020-08-02 23:40:40 -0300

    DataTables demo

--- wallofshame/config/app.php
+++ wallofshame/config/app.php
@@ -161,6 +161,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // DataTables
+        Yajra\DataTables\DataTablesServiceProvider::class,
+        Yajra\DataTables\ButtonsServiceProvider::class,
 
         /*
          * Package Service Providers...
