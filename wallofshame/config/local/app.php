5f91edf2a08d9a3dbb205a5ff7cad0716f5a5a90
DiegoFleitas <diego.fleitas68@gmail.com>
2020-08-02 22:34:52 -0300

    added debugbar for dev

---
+++ wallofshame/config/local/app.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego
+ * Date: 8/2/2020
+ * Time: 10:05 PM
+ */
+
+return [
+    'providers' => append_config([
+        'Barryvdh\Debugbar\ServiceProvider::class'
+    ])
+];
