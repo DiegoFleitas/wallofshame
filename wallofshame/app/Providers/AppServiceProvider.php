207856e54e0d60be426b41236301bebe1c2198d9
DiegoFleitas <diego.fleitas68@gmail.com>
2020-08-25 17:07:11 -0300

    bugfix: Laravel database migration error key too long

--- wallofshame/app/Providers/AppServiceProvider.php
+++ wallofshame/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // HACK: since Beanstalk forces us to choose MySQL 5.5 but Laravel requires MySQL 5.6+
+        Schema::defaultStringLength(191);
     }
 
     /**
